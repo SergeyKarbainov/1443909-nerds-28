b2ce10264aef2d545d3ff47b7060beea538461c5
SergeyKatbainov <barnaul-s@mail.ru>
2020-07-12 01:49:37 +0700

    Delete basket.js

--- basket.js
+++
@@ -1,6 +0,0 @@
- 
-        var link = document.querySelector(".basket");
-        link.addEventListener("click", function (evt) {evt.preventDefault();
-          console.basket("Клик по ссылке корзина");
-        });          
-        
