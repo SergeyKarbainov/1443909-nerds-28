000def0f0092d921534186e553d266d31f74e1da
SergeyKatbainov <barnaul-s@mail.ru>
2020-07-12 01:49:51 +0700

    Create basket.js

---
+++ js/basket.js
@@ -0,0 +1,6 @@
+ 
+        var link = document.querySelector(".basket");
+        link.addEventListener("click", function (evt) {evt.preventDefault();
+          console.basket("Клик по ссылке корзина");
+        });          
+        
